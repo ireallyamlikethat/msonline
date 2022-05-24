80bd2d99642aa4514959b901cabdbd764a3dad66
ireallyamlikethat <32847582+ireallyamlikethat@users.noreply.github.com>
2022-05-24 11:58:14 -0400

    Add files via upload
    traded exchange module for graph module, now including security and mail groups

--- Get-O365GroupMembersExport.ps1
+++ Get-O365GroupMembersExport.ps1
@@ -1,4 +1,4 @@
-#requires -version 4 -modules ExchangeOnlineManagement,ImportExcel 
+#requires -version 4 -modules microsoft.graph,ImportExcel 
 
 <#
 .SYNOPSIS
@@ -44,10 +44,7 @@
 #---------------------------------------------------------[Script Parameters]------------------------------------------------------
 [cmdletbinding()]
 Param (  
-    [Parameter(
-        Mandatory = $true,
-        Position = 0
-    )]
+    [Parameter(Mandatory = $true,Position = 0)]
     $TenantURL,
     [Parameter(Mandatory = $true)]
     $Path,
@@ -58,11 +55,16 @@ Param (
 $ErrorActionPreference = 'Continue'
 
 #import modules
-import-module ExchangeOnlineManagement
 import-module ImportExcel
+import-module microsoft.graph
 
 #Connect to Exchange Online
-Connect-ExchangeOnline -ShowBanner:$False
+##onnect-ExchangeOnline -ShowBanner:$False
+
+#connect to microsoft graph use web auth
+$RequiredScopes = @("Directory.AccessAsUser.All", "Directory.ReadWrite.All","User.Read","Application.Read.All")
+connect-mggraph -Scopes  $RequiredScopes
+
 $tenantBasic = $($TenantURL.Replace('https://','').Replace('/',''))
 $tenantFile = "$tenantBasic-groups-$(get-date -format MMddyyyy).xlsx"
 $ReportFile = join-path $path $tenantFile
@@ -72,38 +74,56 @@ write-verbose "Save Report to - $reportfile" -verbose
 #Get all Office 365 Group
 $curTennant = $TenantURL
 write-verbose "Get all groups for $curTennant " -verbose
-$uGroups = Get-UnifiedGroup |sort-object Alias
-write-verbose "- Found $($ugroups.count) groups"
+$allGroups = get-mggroup |sort-object displayname
+write-verbose "- Found $($allGroups.count) groups"
 
 
-foreach ($group in $ugroups){
+foreach ($group in $allGroups){
   write-verbose "Check users in - $($group.displayname)" -verbose
-    $curUsers = $group | Get-UnifiedGroupLinks -LinkType Member
+    ##$curUsers = $group | Get-UnifiedGroupLinks -LinkType Member
+    $curUsers = Get-MgGroupMember -GroupId $group.id | foreach-object {Get-MgUser -UserId $_.id}    
+    $curOwners = Get-MgGroupOwner -GroupId $group.id | foreach-object {Get-MgUser -UserId $_.id}
     write-verbose "- Found $($curUsers.count) users" -verbose
+    write-verbose "- Found $($curOwners.count) owners" -verbose
  
     $curData = @(
-       #if an owner is not a member include it anyway
-       foreach ($owner in $group.ManagedBy|sort-object ){
-          
-            $curobject = [PSCustomObject]@{
-              GroupName = $group.Displayname
-              State = "OWNER"
-              UserName = $owner
-              UserDisplayName = (get-user $owner ).displayname
-            }
-               
-            $curobject
+        #groups w/out owners or users
+        if ( ($null -eq $curusers) -and ($null -eq $curowners) ){
+          $curobject = [PSCustomObject]@{
+            GroupName = $group.Displayname
+            State = "EMPTY_OWNER"
+            UserName = "EMPTY"
+            UserDisplayName = "NONE"
+            MailEnabled = $group.MailEnabled
+            SecurityEnabled = $group.SecurityEnabled
+          }
+          $curobject
         }
+    
+        #include owners, in case they are not also members
+        foreach ($owner in $curOwners|sort-object ){            
+              $curobject = [PSCustomObject]@{
+                GroupName = $group.Displayname
+                State = "OWNER"
+                UserName = $owner.UserPrincipalName
+                UserDisplayName = $owner.displayname
+                MailEnabled = $group.MailEnabled
+                SecurityEnabled = $group.SecurityEnabled
+              }
+              $curobject
+          }       
         
         #process group members
         foreach ($user in $curUsers){
             $curobject = [PSCustomObject]@{
                 GroupName = $group.Displayname
                 State = "MEMBER"
-                UserName = $user.Name
+                UserName = $user.UserPrincipalName
                 UserDisplayName = $user.Displayname
+                MailEnabled = $group.MailEnabled
+                SecurityEnabled = $group.SecurityEnabled
             }
-            if ($user.name -in $group.managedby){
+            if ($user.UserPrincipalName -in $group.UserPrincipalName){
               $curobject.State  = "OWNER"
             }
             $curobject
@@ -111,20 +131,22 @@ foreach ($group in $ugroups){
     )
     if ($master.ispresent){
         #export to masterlist only        
-        write-verbose -verbose "Write to MasterList only"
+        write-verbose -verbose "Write to worksheet MasterList only $ReportFile"
         
       $curData | sort-object UserName | 
         export-excel -path $ReportFile -WorksheetName MasterList -FreezeTopRow -BoldTopRow -AutoSize -Append
-    }else {
+    } else {
       #export to multiple pages
-      write-verbose -verbose "Write to multiple pages"
+      write-verbose -verbose "Write to worksheet $($group.Displayname) in $ReportFile"
       $curData | sort-object UserName| 
-      export-excel -path $ReportFile -WorksheetName $group.Alias -FreezeTopRow -BoldTopRow -AutoSize
+        export-excel -path $ReportFile -WorksheetName $group.Displayname -FreezeTopRow -BoldTopRow -AutoSize
     }
-    
 }
 
 write-verbose "REPORTING COMPLETE - DISCONNECTING"
 
-#Disconnect Exchange Online
-Disconnect-ExchangeOnline -Confirm:$False
+#disconnect msgraph
+# 'Demo Mail Security Group' and a 'Demo Security Group' in our tenant.
+# 'Demo Mail Security Empty Group' so i can see an owner of an empty group
+# 'Demo Security Empty Group' so i can see an owner of an empty group
+Disconnect-MgGraph
