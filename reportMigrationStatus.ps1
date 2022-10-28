ad0f88ed4f045a74c86eabfa4fe2f9bbab60e0ad
ireallyamlikethat <32847582+ireallyamlikethat@users.noreply.github.com>
2022-10-28 13:23:05 -0400

    Add files via upload
    loop skipped items

--- reportMigrationStatus.ps1
+++ reportMigrationStatus.ps1
@@ -27,16 +27,16 @@ $curData = @(
     foreach ($batch in $BatchId){
         write-verbose -verbose "Process Batch: $batch"
         $users = Get-MigrationUser -ResultSize Unlimited -BatchId $batch
-        foreach ($user in $users){
-            write-verbose "Process $($user.identity)"
-            $userStats = Get-MigrationUserStatistics -Identity $user.identity -IncludeSkippedItems -IncludeReport | 
-                select-object Identity , IsValid, Subject, Sender, Recipient, Kind, DateSent,  DateReceived, FolderName, MessageSize, ScoringClassifications
-
-        $userStats
+        foreach ($user in $users){            
+            write-verbose -verbose "Process $($user.identity)"
+            $userStats = Get-MigrationUserStatistics -Identity $user.identity -IncludeSkippedItems -IncludeReport 
+                foreach ($skItem in $userstats.skippeditems){
+                    $skItem |select-object Identity , Kind, ScoringClassifications, FolderName, Sender, Recipient, Subject, MessageSize, DateSent, DateReceived, Isvalid, Failure
+                }
         }        
     }
 )
 
 write-verbose -verbose "Export validation file - $reportfile"
 $curData | sort-object Identity | 
-        export-excel -path $ReportFile -WorksheetName mailboxes -FreezeTopRow -BoldTopRow -AutoSize -Append
+        export-excel -path $ReportFile -WorksheetName migration -FreezeTopRow -BoldTopRow -AutoSize -Append
